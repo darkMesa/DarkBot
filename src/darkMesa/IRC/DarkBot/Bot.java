465d403c1ccffe18a7d330a8399fd959bfe08bcc
jmgr2007 <michealmyersreal@gmail.com>
2013-02-23 21:25:22 -0600

    Fix timeout bug

--- src/darkMesa/IRC/DarkBot/Bot.java
+++ src/darkMesa/IRC/DarkBot/Bot.java
@@ -22,21 +22,21 @@ public class Bot extends PircBot {
 		log("Joining channel...");
 		this.joinChannel();
 	}
-	
+
 	public Runnable r = new Runnable() {
 		@Override
 		public void run() {
 			Scanner in = new Scanner(System.in);
-			
-			while(true) {
-				if(in.hasNextLine()) {
+
+			while (true) {
+				if (in.hasNextLine()) {
 					sendMessage(main.channel, in.nextLine());
 				}
 			}
 		}
 	};
 	public Thread t = new Thread(r);
-	
+
 	@Override
 	protected void onConnect() {
 		load();
@@ -47,137 +47,148 @@ public class Bot extends PircBot {
 	@Override
 	protected void onDisconnect() {
 		saveAll();
-
 		super.onDisconnect();
+		try {
+			this.reconnect();
+			load();
+			this.joinChannel();
+			this.sendMessage("nickserv", "identify " + main.password);
+		} catch (NickAlreadyInUseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IrcException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	protected void onJoin(String channel, String sender, String login, String hostname) {
-		if(main.ops.contains(sender)) {
+	protected void onJoin(String channel, String sender, String login,
+			String hostname) {
+		if (main.ops.contains(sender)) {
 			this.op(channel, sender);
-		}else if(main.voices.contains(sender)) {
+		} else if (main.voices.contains(sender)) {
 			this.voice(channel, sender);
-		}else if(main.users.contains(sender)){
-			this.sendNotice(sender, "Welcome back " + sender);
-		}else{
-			this.sendNotice(sender, "If you have a question, don't hesitiate to ask but remember, we may be busy or unavalible.");
-			addUser(sender);
+		} else if (main.users.contains(sender)) {
 		}
 		super.onJoin(channel, sender, login, hostname);
 	}
 
 	@Override
-	protected void onMessage(String channel, String sender, String login, String hostname, String message) {
+	protected void onMessage(String channel, String sender, String login,
+			String hostname, String message) {
 		Boolean perm = false;
 		User[] users = this.getUsers(channel);
 
-		for(int i = 0; i < users.length; i++) {
-			if(users[i].isOp() && users[i].getNick().equals(sender)) {
+		for (int i = 0; i < users.length; i++) {
+			if (users[i].isOp() && users[i].getNick().equals(sender)) {
 				perm = true;
 			}
 		}
 
 		String[] args = message.split(" ");
 
-		if(perm) {
-			if(args[0].equalsIgnoreCase(main.prefix + "save")) {
-				if(args.length < 2) {
+		if (perm) {
+			if (args[0].equalsIgnoreCase(main.prefix + "save")) {
+				if (args.length < 2) {
 					saveAll();
 					this.sendNotice(sender, "Saved files!");
 				} else {
-					if(args[1].equalsIgnoreCase("ops")) {
+					if (args[1].equalsIgnoreCase("ops")) {
 						saveOps();
 						this.sendNotice(sender, "Saved OPs file!");
-					} else if(args[1].equalsIgnoreCase("voices")) {
+					} else if (args[1].equalsIgnoreCase("voices")) {
 						saveVoices();
 						this.sendNotice(sender, "Saved Voices file!");
 					}
 				}
 			}
 
-			if(args[0].equalsIgnoreCase(main.prefix + "op")) {
+			if (args[0].equalsIgnoreCase(main.prefix + "op")) {
 				addOp(args[1]);
 				this.sendNotice(sender, "OP'd \"" + args[1] + "\"!");
 			}
 
-			if(args[0].equalsIgnoreCase(main.prefix + "deop")) {
+			if (args[0].equalsIgnoreCase(main.prefix + "deop")) {
 				delOp(args[1]);
 				this.sendNotice(sender, "DeOP'd \"" + args[1] + "\"!");
 			}
 
-			if(args[0].equalsIgnoreCase(main.prefix + "voice")) {
+			if (args[0].equalsIgnoreCase(main.prefix + "voice")) {
 				addVoice(args[1]);
 				this.sendNotice(sender, "Voice'd \"" + args[1] + "\"!");
 			}
 
-			if(args[0].equalsIgnoreCase(main.prefix + "devoice")) {
+			if (args[0].equalsIgnoreCase(main.prefix + "devoice")) {
 				delVoice(args[1]);
 				this.sendNotice(sender, "DeVoice'd \"" + args[1] + "\"!");
 			}
 		}
 
-		if(args[0].equalsIgnoreCase(main.prefix + "help")) {
-			this.sendNotice(sender, "Commands Avaiable: !help, !git");
-			this.sendNotice(sender, "OP Only Commands: !op, !deop, !voice, !devoice, !save");
+		if (args[0].equalsIgnoreCase(main.prefix + "help")) {
+			this.sendNotice(sender, "Commands Avaiable: " + main.prefix + "help, ");
+			this.sendNotice(sender,
+					"OP Only Commands: " + main.prefix + "op, " + main.prefix + "deop, " + main.prefix + "voice, " + main.prefix + "devoice, " + main.prefix + "save");
 		}
-		
-		if(args[0].equalsIgnoreCase(main.prefix + "git")) {
-			this.sendNotice(sender, "Our GitHub is located at: https://github.com/darkMesa");
-		}
-		
-		if(args[0].equalsIgnoreCase(main.prefix + "paste")) {
-			this.sendNotice(channel, "To show us any errors please go to: http://pastebin.com/");
+
+		if (args[0].equalsIgnoreCase(main.prefix + "paste")) {
+			this.sendMessage(channel, "http://pastebin.com/");
 		}
 
 		super.onMessage(channel, sender, login, hostname, message);
 	}
 
-	protected void onNotice(String sender, String sourceLogin, String sourceHostname, String target, String notice) {
+	protected void onNotice(String sender, String sourceLogin,
+			String sourceHostname, String target, String notice) {
 		Boolean perm = false;
 		User[] users = this.getUsers(main.channel);
 
-		for(int i = 0; i < users.length; i++) {
-			if(users[i].isOp() && users[i].getNick().equals(sender)) {
+		for (int i = 0; i < users.length; i++) {
+			if (users[i].isOp() && users[i].getNick().equals(sender)) {
 				perm = true;
 			}
 		}
 
 		String[] args = notice.split(" ");
-		if(perm){
-			if(args[0].equalsIgnoreCase("!help")) {
-				this.sendNotice(sender, "!topic <topic> -- set topic in " + main.channel + ".");
-				this.sendNotice(sender, "!say <message> -- send a channel-wide notice to " + main.channel + ".");
-				this.sendNotice(sender, "!notice <user> <message> -- send message to <user>");
+		
+		if (perm) {
+			if (args[0].equalsIgnoreCase(main.prefix + "help")) {
+				this.sendNotice(sender, main.prefix + "topic <topic> -- set topic in " + main.channel + ".");
+				this.sendNotice(sender, main.prefix + "say <message> -- send a channel-wide notice to " + main.channel + ".");
+				this.sendNotice(sender, main.prefix + "notice <user> <message> -- send message to <user>");
 			}
-			if(args[0].equalsIgnoreCase("!topic")) {
+			if (args[0].equalsIgnoreCase(main.prefix + "topic")) {
 				String topic = "";
-				for(int i = 1; i < args.length; i++){
+				for (int i = 1; i < args.length; i++) {
 					topic = topic + args[i] + " ";
 				}
 				topic = topic.trim();
 				this.setTopic(main.channel, topic);
 			}
-			if(args[0].equalsIgnoreCase("!say")) {
+			if (args[0].equalsIgnoreCase(main.prefix + "say")) {
 				String msg = "";
-				for(int i = 1; i < args.length; i++){
+				for (int i = 1; i < args.length; i++) {
 					msg = msg + args[i] + " ";
 				}
 				msg = msg.trim();
-				this.sendNotice(main.channel, msg);
+				this.sendMessage(main.channel, msg);
 			}
-			if(args[0].equalsIgnoreCase("!notice")) {
-				if(args.length == 1) {
+			if (args[0].equalsIgnoreCase(main.prefix + "notice")) {
+				if (args.length == 1) {
 					this.sendNotice(sender, "Please specify user and message");
-				} else if(args.length == 2) {
+				} else if (args.length == 2) {
 					this.sendNotice(sender, "Please specify the message");
 				} else {
 					String msg = "";
-					for(int i = 2; i < args.length; i++) {
+					for (int i = 2; i < args.length; i++) {
 						msg = msg + args[i] + " ";
 					}
 					msg = msg.trim();
-					for(int i = 0; i < users.length; i++) {
-						if(users[i].getNick().equals(args[1])) {
+					for (int i = 0; i < users.length; i++) {
+						if (users[i].getNick().equals(args[1])) {
 							this.sendNotice(users[i].getNick(), msg);
 						}
 					}
@@ -186,52 +197,54 @@ public class Bot extends PircBot {
 		}
 	}
 
-	protected void onPrivateMessage(String sender, String login, String hostname, String notice) {
+	protected void onPrivateMessage(String sender, String login,
+			String hostname, String notice) {
 		Boolean perm = false;
 		User[] users = this.getUsers(main.channel);
 
-		for(int i = 0; i < users.length; i++) {
-			if(users[i].isOp() && users[i].getNick().equals(sender)) {
+		for (int i = 0; i < users.length; i++) {
+			if (users[i].isOp() && users[i].getNick().equals(sender)) {
 				perm = true;
 			}
 		}
 
 		String[] args = notice.split(" ");
-		if(perm){
-			if(args[0].equalsIgnoreCase(main.prefix + "help")) {
-				this.sendMessage(sender, "!topic <topic> -- set topic in " + main.channel + ".");
-				this.sendMessage(sender, "!say <message> -- send a channel-wide notice to " + main.channel + ".");
-				this.sendMessage(sender, "!notice <user> <message> -- send a notice to <user>");
+		
+		if (perm) {
+			if (args[0].equalsIgnoreCase(main.prefix + "help")) {
+				this.sendMessage(sender,  main.prefix + "topic <topic> -- set topic in " + main.channel + ".");
+				this.sendMessage(sender, main.prefix + "say <message> -- send a channel-wide notice to " + main.channel + ".");
+				this.sendMessage(sender, main.prefix + "notice <user> <message> -- send a notice to <user>");
 			}
-			if(args[0].equalsIgnoreCase(main.prefix + "topic")) {
+			if (args[0].equalsIgnoreCase(main.prefix + "topic")) {
 				String topic = "";
-				for(int i = 1; i < args.length; i++){
+				for (int i = 1; i < args.length; i++) {
 					topic = topic + args[i] + " ";
 				}
 				topic = topic.trim();
 				this.setTopic(main.channel, topic);
 			}
-			if(args[0].equalsIgnoreCase(main.prefix + "say")) {
+			if (args[0].equalsIgnoreCase("say")) {
 				String msg = "";
-				for(int i = 1; i < args.length; i++){
+				for (int i = 1; i < args.length; i++) {
 					msg = msg + args[i] + " ";
 				}
 				msg = msg.trim();
-				this.sendNotice(main.channel, msg);
+				this.sendMessage(main.channel, msg);
 			}
-			if(args[0].equalsIgnoreCase(main.prefix + "notice")) {
-				if(args.length == 1) {
+			if (args[0].equalsIgnoreCase(main.prefix + "notice")) {
+				if (args.length == 1) {
 					this.sendMessage(sender, "Please specify user and message");
-				} else if(args.length == 2) {
+				} else if (args.length == 2) {
 					this.sendMessage(sender, "Please specify the message");
 				} else {
 					String msg = "";
-					for(int i = 2; i < args.length; i++) {
+					for (int i = 2; i < args.length; i++) {
 						msg = msg + args[i] + " ";
 					}
 					msg = msg.trim();
-					for(int i = 0; i < users.length; i++) {
-						if(users[i].getNick().equals(args[1])) {
+					for (int i = 0; i < users.length; i++) {
+						if (users[i].getNick().equals(args[1])) {
 							this.sendNotice(users[i].getNick(), msg);
 						}
 					}
@@ -239,6 +252,7 @@ public class Bot extends PircBot {
 			}
 		}
 	}
+
 	public void addOp(String s) {
 		this.op(main.channel, s);
 		log("OP'd \"" + s + "\"!");
@@ -292,14 +306,15 @@ public class Bot extends PircBot {
 			this.connect(main.server);
 			this.sendMessage("nickserv", "identify " + main.password);
 			log("Connected!");
-		} catch(NickAlreadyInUseException e1) {
+		} catch (NickAlreadyInUseException e1) {
 			log("Nick in use, GHOSTing...");
 			this.setAutoNickChange(true);
 			joinServer();
-			this.sendMessage("nickserv", "ghost " + main.name + " " + main.password);
+			this.sendMessage("nickserv", "ghost " + main.name + " "
+					+ main.password);
 			this.changeNick(main.name);
 			log("Nick recovered!");
-		} catch(IOException | IrcException e2) {
+		} catch (IOException | IrcException e2) {
 			e2.printStackTrace();
 		}
 	}
@@ -309,10 +324,10 @@ public class Bot extends PircBot {
 		log("Joined channel \"" + main.channel + "\"!");
 
 		User[] users = this.getUsers(main.channel);
-		for(int i = 0; i < users.length; i++) {
+		for (int i = 0; i < users.length; i++) {
 			String user = users[i].getNick();
 
-			if(main.ops.contains(user)) {
+			if (main.ops.contains(user)) {
 				this.op(main.channel, user);
 				log("OP'd \"" + user + "\"!");
 			}
@@ -324,7 +339,7 @@ public class Bot extends PircBot {
 		try {
 			SLAPI.save(main.ops, "ops.bin");
 			log("Saved \"ops.bin\"!");
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -333,7 +348,7 @@ public class Bot extends PircBot {
 		try {
 			SLAPI.save(main.voices, "voices.bin");
 			log("Saved \"voices.bin\"!");
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -342,7 +357,7 @@ public class Bot extends PircBot {
 		try {
 			SLAPI.save(main.users, "users.bin");
 			log("Saved \"users.bin\"!");
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -355,7 +370,7 @@ public class Bot extends PircBot {
 			log("Saved \"voices.bin\"!");
 			SLAPI.save(main.users, "users.bin");
 			log("Saved \"users.bin\"!");
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -366,14 +381,14 @@ public class Bot extends PircBot {
 			log("Loaded \"ops.bin\"!");
 			main.voices = SLAPI.load("voices.bin");
 			log("Loaded \"voices.bin\"!");
-		} catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			log("\"ops.bin\" and/or \"voices.bin\" not found!");
 			log("Generating new files...");
 			saveAll();
 			log("New files generated!");
-		} catch(ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
--- src/darkMesa/IRC/DarkBot/DarkBot.java
+++ src/darkMesa/IRC/DarkBot/DarkBot.java
@@ -5,10 +5,10 @@ import java.util.Set;
 
 public class DarkBot {
 	
-	public String name = "DarkBot";
-	public String password = "password"; //:D
-	public String server = "irc.esper.net";
-	public String channel = "#darkmesa";
+	public String name = "Bot";
+	public String password = "Password"; //:D
+	public String server = "Server";
+	public String channel = "#Channel";
 	public Set<String> ops = new HashSet<String>();
 	public Set<String> voices = new HashSet<String>();
 	public Set<String> users = new HashSet<String>();
